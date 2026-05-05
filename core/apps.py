8962767055c73961268aa50e47bbf12a7919dd7b
bhavikmv <bhavikmv5@gmail.com>
2026-05-05 14:41:29 +0530

    Update apps.py

--- core/apps.py
+++ core/apps.py
@@ -3,14 +3,4 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     name = 'core'
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
-if not User.objects.filter(username='bhavik').exists():
-    User.objects.create_superuser(
-        fristname='bhavik',
-        lastname='vasavada',
-        email='bhavikmv5@gmail.com',
-        password='bhavik@123'
-    )
