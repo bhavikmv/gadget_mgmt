895abc53fdcad06d58fa4523b6d928f7b345d39a
bhavikmv <bhavikmv5@gmail.com>
2026-05-05 14:30:57 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -199,3 +199,14 @@ def admin_mark_returned(request, pk):
     return redirect('admin_requests')
 
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='admin',
+        email='admin@gmail.com',
+        password='admin123'
+    )
+
