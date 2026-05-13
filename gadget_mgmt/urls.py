067027c2454c85baecfd089274204988f5bd1afe
PritiV <pritivasavada.12@gmail.com>
2026-05-13 13:34:51 +0530

    sdcs ffg sdfs

--- gadget_mgmt/urls.py
+++ gadget_mgmt/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +12,9 @@ urlpatterns = [
     
     # Gadgets and core student/admin routes handled by gadgets app
     path('', include('gadgets.urls')),
+]
+
+# Serve media files on Render (workaround for small projects)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
